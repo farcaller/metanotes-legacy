a3880d118adfd247dccc691f5759eef39fd82d56
Vladimir Pouzanov <farcaller@gmail.com>
2020-11-08 19:01:16 +0000

    Remove the old web/mobile apps (partially superseded by core for now)

--- packages/metanotes-mobile/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
-
-const config = createMetroConfiguration(__dirname);
-
-config.resolver.extraNodeModules.path = require('node-libs-react-native').path;
-
-module.exports = config;
--- packages/metanotes-web/README.md
+++
@@ -1,3 +0,0 @@
-# metanotes-web
-
-The web frontend for metanotes based on create-react-app.

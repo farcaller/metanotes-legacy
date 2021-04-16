88284b5bbef0b85e22afa0346a2f4b1d72b41633
Vladimir Pouzanov <farcaller@gmail.com>
2021-04-16 13:11:06 +0100

    Clean up global types (only used for scribbles)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -12,34 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-declare const core: {
-  createCachedSelector: typeof import('re-reselect').createCachedSelector,
-  createSelector: typeof import('@reduxjs/toolkit').createSelector,
-  useSelector: typeof import('react-redux').useSelector,
-  useDispatch: typeof import('react-redux').useDispatch,
-  selectScribbleById: typeof import('../frontend/store/features/scribbles/index').selectScribbleById,
-  selectScribbleByTitle: typeof import('../frontend/store/features/scribbles/selectors').selectScribbleByTitle,
-  selectScribblesByTag: typeof import('../frontend/store/features/scribbles/tagging').selectScribblesByTag,
-  selectAllScribbles: typeof import('../frontend/store/features/scribbles/index').selectAllScribbles,
-  useScribble: typeof import('../frontend/store/features/scribbles/useScribble').useScribble,
-  fetchScribble: typeof import('../frontend/store/features/scribbles/index').fetchScribble,
-  removeScribble: typeof import('../frontend/store/features/scribbles/index').removeScribble,
-  updateScribbleBody: typeof import('../frontend/store/features/scribbles/index').updateScribbleBody,
-  updateScribbleAttributes: typeof import('../frontend/store/features/scribbles/index').updateScribbleAttributes,
-  removeScribbleAttributes: typeof import('../frontend/store/features/scribbles/index').removeScribbleAttributes,
-  commitDraft: typeof import('../frontend/store/features/scribbles/index').commitDraft,
-  equals: typeof import('deep-equal'),
-  loadScribbleComponent: typeof import('../frontend/store/features/scribbles/useScribble').loadScribbleComponent,
-  UseScribbleContext: typeof import('../frontend/store/features/scribbles/useScribble').UseScribbleContext,
-  ScribbleResolverContext:
-    typeof import('../frontend/store/features/scribbles/ScribbleResolverContext').ScribbleResolverContext,
-  createDraftScribble: typeof import('../frontend/store/features/scribbles/index').createDraftScribble,
-  ulid: typeof import('ulid'),
-  syncScribble: typeof import('../frontend/store/features/scribbles/index').syncScribble,
-  resetSyncError: typeof import('../frontend/store/features/scribbles/index').resetSyncError,
-  selectLastSyncError: typeof import('../frontend/store/features/scribbles/index').selectLastSyncError,
-};
-
-declare const components: {
-  PropTypes: typeof import('prop-types'),
-};
+declare const React: typeof import('react');
